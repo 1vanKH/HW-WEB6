19c3cc7aa893120abd68b08566bf66e673cdb66b
1vanKH <savchenko1208ivan@gmail.com>
2024-04-21 15:13:01 +0300

    Update query_2.sql

--- query_2.sql
+++ query_2.sql
@@ -2,7 +2,7 @@ SELECT students.name, AVG(grades.grade) AS average_grade
 FROM students
 JOIN grades ON students.id = grades.student_id
 JOIN subjects ON grades.subject_id = subjects.id
-WHERE subjects.name = 'four'
+WHERE subjects.name = 'Mathematics'
 GROUP BY students.id, students.name
 ORDER BY average_grade DESC
 LIMIT 1;
