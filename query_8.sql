374111b25d5212caba816f59b760bdd3b29d8357
1vanKH <savchenko1208ivan@gmail.com>
2024-04-21 15:12:54 +0300

    Update query_8.sql

--- query_8.sql
+++ query_8.sql
@@ -2,4 +2,4 @@ SELECT AVG(grades.grade) AS average_grade
 FROM grades
 JOIN subjects ON grades.subject_id = subjects.id
 JOIN teachers ON subjects.teacher_id = teachers.id
-WHERE teachers.name = 'Justin Hogan';
+WHERE teachers.name = 'Christina Phillips';
