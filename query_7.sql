e8cbc5835baa358cac9392fb1ee4b8f6727fbd5a
1vanKH <savchenko1208ivan@gmail.com>
2024-04-21 15:12:52 +0300

    Update query_7.sql

--- query_7.sql
+++ query_7.sql
@@ -3,4 +3,4 @@ FROM students
 JOIN groups ON students.group_id = groups.id
 JOIN grades ON students.id = grades.student_id
 JOIN subjects ON grades.subject_id = subjects.id
-WHERE groups.name = 'but' AND subjects.name = 'four';
+WHERE groups.name = 'open' AND subjects.name = 'Mathematics';
