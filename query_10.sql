f8c51083b0ae8d1e222833583ca0df508b63d082
1vanKH <savchenko1208ivan@gmail.com>
2024-04-21 15:12:45 +0300

    Update query_10.sql

--- query_10.sql
+++ query_10.sql
@@ -3,4 +3,4 @@ FROM subjects
 JOIN grades ON subjects.id = grades.subject_id
 JOIN students ON grades.student_id = students.id
 JOIN teachers ON subjects.teacher_id = teachers.id
-WHERE students.name = 'Megan Morales' AND teachers.name = 'Justin Hogan';
+WHERE students.name = 'Christina Phillips' AND teachers.name = 'Joshua Fox';
