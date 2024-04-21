d96761dfe9e1a91e2b4add4aa63939a426a1fab4
1vanKH <savchenko1208ivan@gmail.com>
2024-04-21 15:12:47 +0300

    Update query_12.sql

--- query_12.sql
+++ query_12.sql
@@ -3,6 +3,6 @@ FROM grades
 JOIN subjects ON grades.subject_id = subjects.id
 JOIN students ON grades.student_id = students.id
 JOIN groups ON students.group_id = groups.id
-WHERE subjects.name = 'four' AND groups.name = 'but'
+WHERE subjects.name = 'Engineering' AND groups.name = 'something'
 ORDER BY grades.date_received DESC
 LIMIT 1;
